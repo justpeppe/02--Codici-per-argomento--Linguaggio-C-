6e501c3960178e19c5d40a7ac18c6fce0e731efa
Peppe <trimarchigiuseppe@outlook.it>
2025-05-11 16:40:08 +0200

    Definizione costanti

---
+++ FI A 09 - Array e stringhe/00_Definizioni_Costanti.c
@@ -0,0 +1,26 @@
+// Esempio di utilizzo della costante nel calcolo dell'area di un cerchio
+#include <stdio.h>
+#include <math.h>
+
+// Come si definiscono le costanti in C? 
+// Utilizziamo il comando #define per definire le costanti
+// Definiziamo la costante PiGreco
+#define PI 3.14159 
+
+
+int main() {
+    // Dichiarazione delle variabili double cioè con virgola mobile
+    float radius, area; 
+
+    // Chiediamo all'utente di inserire il raggio del cerchio
+    printf("Inserisci il raggio del cerchio: ");
+    scanf("%f", &radius);
+
+    // Calcoliamo l'area del cerchio utilizzando la costante PI
+    area = PI * radius * radius;
+
+    // Stampiamo l'area del cerchio
+    printf("L'area del cerchio con raggio scelto %.2f è: %.2f\n", radius, area);
+
+    return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Per compilare si utilizza il seguente comando:
+gcc "nome_file.c" -o "nome_eseguibile"
+
+Per aprire il file compilato si usa
+./mio_programma
